ac9409da0265c7a2840e298648f8b2b8ef7fde2c
ipink <1242849166@qq.com>
2023-10-22 16:16:45 +0800

    fix

--- src/pages/login/forget.vue
+++ src/pages/login/forget.vue
@@ -1,6 +1,6 @@
 <template>
     <view>
-        11
+        Forget Account Password
     </view>
 </template>
 
--- src/pages/login/register.vue
+++ src/pages/login/register.vue
@@ -1,6 +1,6 @@
 <template>
     <view>
-        11
+        Sign Up
     </view>
 </template>
 
